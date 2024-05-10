3633fe8544b399a5d282b48a79c1c5a27f4ce291
Tim203 <mctim203@gmail.com>
2024-05-10 15:42:42 +0200

    Auto sync daily and publish to opencollab repo
    (cherry picked from commit 2dca88393ba146727514f428d1f64359546c3fcb)

--- build-logic/src/main/kotlin/velocity-publish.gradle.kts
+++ build-logic/src/main/kotlin/velocity-publish.gradle.kts
@@ -8,10 +8,10 @@ extensions.configure<PublishingExtension> {
         maven {
             credentials(PasswordCredentials::class.java)
 
-            name = if (version.toString().endsWith("SNAPSHOT")) "paperSnapshots" else "paper" // "paper" is seemingly not defined
-            val base = "https://artifactory.papermc.io/artifactory"
-            val releasesRepoUrl = "$base/releases/"
-            val snapshotsRepoUrl = "$base/snapshots/"
+            name = "geysermc"
+            val base = "https://repo.opencollab.dev/maven"
+            val releasesRepoUrl = "$base-releases/"
+            val snapshotsRepoUrl = "$base-snapshots/"
             setUrl(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
         }
     }
